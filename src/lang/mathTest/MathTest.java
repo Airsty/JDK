2d30856d86c91a7523ebdc0eed49b950a3a01aae
Airsty <airsty@163.com>
2019-01-05 22:02:55 +0800

    JDK

--- src/lang/mathTest/MathTest.java
+++ src/lang/mathTest/MathTest.java
@@ -2,5 +2,35 @@ package lang.mathTest;
 
 public class MathTest {
 
+  public static void main(String[] args) {
+    Double dou = -10.1;
+    double douAbs = Math.abs(dou);
+    /**
+     * abs 绝对值
+     *
+     * acos(double a) 反余弦
+     *
+     * asin(double a) 反正弦
+     *
+     * atan(double a) 反正切
+     *
+     * atan2(doule y,double x) 将矩形坐标 (x, y) 转换成极坐标 (r, theta)，返回所得角 theta。
+     *
+     * cbrt(double a) 返回double的立方根
+     *
+     * pow(double a, double b) 返回第一个参数的第二个参数次幂的值。
+     *
+     * random() 但会带正号的double值，该值大于等于0.0且小于1.0
+     *
+     * rint(double) 返回最接近参数并等于某一整数的double值
+     *
+     * round(double a) 返回接近参数的long
+     *
+     * round(float a) 返回接近参数的int
+     *
+     *
+     *
+     */
+  }
 
 }
---
+++ src/lang/objectTest/ObjectTest.java
@@ -0,0 +1,36 @@
+package lang.objectTest;
+
+/**
+ * 方法摘要
+ *
+ * clone()
+ *
+ * equals(Object obj)
+ *
+ * finalize()
+ *
+ * getClass()
+ *
+ * hashCode()
+ *
+ * notify()
+ *
+ * notifyAll()
+ *
+ * toString()
+ *
+ * wait()
+ *
+ * wait(long timeout)
+ *
+ * wait(long timeout,int nanos) 在其他线程调用此对象的 notify() 方法或 notifyAll() 方法，
+ * 或者其他某个线程中断当前线程，或者已超过某个实际时间量前，导致当前线程等待。
+ * timeout - 要等待的最长时间（以毫秒为单位）。
+ * nanos - 额外时间（以毫微秒为单位，范围是 0-999999）。
+ *
+ *
+ */
+public class ObjectTest {
+
+
+}
